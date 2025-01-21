db9845beda35f7796abf023aa27efb928e6d0ebe
Christofer Galdino Bernardo <christofergaldino2@hotmail.com>
2025-01-21 13:47:04 -0300

    Creates csv helpers

---
+++ Helpers/CsvReaderHelper.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using QuorumTest.Mappers;
+using System.Globalization;
+using System.Reflection;
+
+namespace QuorumTest.Helpers
+{
+    public class CsvReaderHelper
+    {
+        public static List<T> ReadCsv<T>(string filePath)
+        {
+            using var reader = new StreamReader(filePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var mapType = Assembly
+                .GetAssembly(typeof(BaseMap<T>))?
+                .GetTypes()
+                .Where(map => map.IsClass && map.IsSubclassOf(typeof(BaseMap<T>)))
+                .FirstOrDefault();
+
+            csv.Context.RegisterClassMap(mapType);
+            
+            var records = csv.GetRecords<T>().ToList();
+            return records;
+        }
+    }
+}
---
+++ Helpers/CsvWriterHelper.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using QuorumTest.Mappers;
+using System.Globalization;
+using System.Reflection;
+
+namespace QuorumTest.Helpers
+{
+    public class CsvWriterHelper
+    {
+        public static void WriteCsv<T>(List<T> records, string filePath)
+        {
+            using var writer = new StreamWriter(filePath);
+            using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            var mapType = Assembly
+                .GetAssembly(typeof(BaseMap<T>))?
+                .GetTypes()
+                .Where(map => map.IsClass && map.IsSubclassOf(typeof(BaseMap<T>)))
+                .FirstOrDefault();
+
+            csv.Context.RegisterClassMap(mapType);
+
+            csv.WriteRecords(records);
+        }
+    }
+}
